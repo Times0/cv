7eb5fcd5a5e523153517a8906550cda647ffc34e
Times0 <45049767+Times0@users.noreply.github.com>
2024-01-29 12:18:09 +0100

    maybe fixed the bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,31 +11,28 @@ import { useState } from "react";
 import { RESUME_DATA as RESUME_DATA_EN } from "../data/resume-data-en";
 import { RESUME_DATA as RESUME_DATA_FR } from "../data/resume-data-fr";
 
+type Language = "EN" | "FR";
+
 export default function Page() {
-  const [language, setLanguage] = useState("EN");
-  const resumeData = language === "EN" ? RESUME_DATA_EN : RESUME_DATA_FR;
-  let data = resumeData;
+  const [language, setLanguage] = useState("FR");
+  let resumeData = language === "EN" ? RESUME_DATA_EN : RESUME_DATA_FR;
 
-  const changeToFr = () => {
-    setLanguage("FR");
-  };
-
-  const changeToEn = () => {
-    setLanguage("EN");
+  const switchLanguageTo = (lang: Language) => {
+    setLanguage(lang);
   };
   return (
     <div>
       <main className="container relative mx-auto scroll-my-12 overflow-auto p-4 print:p-12 md:p-16">
         <div className="absolute right-0 top-0 flex gap-x-2 p-2">
           <button
-            onClick={changeToFr}
+            onClick={() => switchLanguageTo("FR")}
             className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-200 hover:bg-gray-300"
           >
             <img src="assets/icons8-france-96.png" alt="French" />
           </button>
 
           <button
-            onClick={changeToEn}
+            onClick={() => switchLanguageTo("EN")}
             className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-200 hover:bg-gray-300"
           >
             <img src="assets/icons8-great-britain-96.png" alt="English" />
@@ -45,46 +42,46 @@ export default function Page() {
         <section className="mx-auto w-full max-w-2xl space-y-8 bg-white print:space-y-6">
           <div className="flex items-center justify-between">
             <div className="flex-1 space-y-1.5">
-              <h1 className="text-2xl font-bold">{data.name}</h1>
+              <h1 className="text-2xl font-bold">{resumeData.name}</h1>
               <p className="max-w-md text-pretty font-mono text-sm text-muted-foreground">
-                {data.about}
+                {resumeData.about}
               </p>
               <p className="max-w-md items-center text-pretty font-mono text-xs text-muted-foreground">
                 <a
                   className="inline-flex gap-x-1.5 align-baseline leading-none hover:underline"
-                  href={data.locationLink}
+                  href={resumeData.locationLink}
                   target="_blank"
                 >
                   <GlobeIcon className="size-3" />
-                  {data.location}
+                  {resumeData.location}
                 </a>
               </p>
               <div className="flex gap-x-1 pt-1 font-mono text-sm text-muted-foreground print:hidden">
-                {data.contact.email ? (
+                {resumeData.contact.email ? (
                   <Button
                     className="size-8"
                     variant="outline"
                     size="icon"
                     asChild
                   >
-                    <a href={`mailto:${data.contact.email}`}>
+                    <a href={`mailto:${resumeData.contact.email}`}>
                       <MailIcon className="size-4" />
                     </a>
                   </Button>
                 ) : null}
-                {data.contact.tel ? (
+                {resumeData.contact.tel ? (
                   <Button
                     className="size-8"
                     variant="outline"
                     size="icon"
                     asChild
                   >
-                    <a href={`tel:${data.contact.tel}`}>
+                    <a href={`tel:${resumeData.contact.tel}`}>
                       <PhoneIcon className="size-4" />
                     </a>
                   </Button>
                 ) : null}
-                {data.contact.social.map((social) => (
+                {resumeData.contact.social.map((social) => (
                   <Button
                     key={social.name}
                     className="size-8"
@@ -99,33 +96,35 @@ export default function Page() {
                 ))}
               </div>
               <div className="hidden flex-col gap-x-1 font-mono text-sm text-muted-foreground print:flex">
-                {data.contact.email ? (
-                  <a href={`mailto:${data.contact.email}`}>
-                    <span className="underline">{data.contact.email}</span>
+                {resumeData.contact.email ? (
+                  <a href={`mailto:${resumeData.contact.email}`}>
+                    <span className="underline">
+                      {resumeData.contact.email}
+                    </span>
                   </a>
                 ) : null}
-                {data.contact.tel ? (
-                  <a href={`tel:${data.contact.tel}`}>
-                    <span className="underline">{data.contact.tel}</span>
+                {resumeData.contact.tel ? (
+                  <a href={`tel:${resumeData.contact.tel}`}>
+                    <span className="underline">{resumeData.contact.tel}</span>
                   </a>
                 ) : null}
               </div>
             </div>
 
             <Avatar className="size-28">
-              <AvatarImage alt={data.name} src={data.avatarUrl} />
-              <AvatarFallback>{data.initials}</AvatarFallback>
+              <AvatarImage alt={resumeData.name} src={resumeData.avatarUrl} />
+              <AvatarFallback>{resumeData.initials}</AvatarFallback>
             </Avatar>
           </div>
           <Section>
             <h2 className="text-xl font-bold">About</h2>
             <p className="text-pretty font-mono text-sm text-muted-foreground">
-              {data.summary}
+              {resumeData.summary}
             </p>
           </Section>
           <Section>
             <h2 className="text-xl font-bold">Work Experience</h2>
-            {data.work.map((work) => {
+            {resumeData.work.map((work) => {
               return (
                 <Card key={work.company}>
                   <CardHeader>
@@ -165,7 +164,7 @@ export default function Page() {
           </Section>
           <Section>
             <h2 className="text-xl font-bold">Education</h2>
-            {data.education.map((education) => {
+            {resumeData.education.map((education) => {
               return (
                 <Card key={education.school}>
                   <CardHeader>
@@ -187,7 +186,7 @@ export default function Page() {
           <Section>
             <h2 className="text-xl font-bold">Languages</h2>
             <div className="flex flex-wrap gap-1">
-              {data.languages.map((language) => {
+              {resumeData.languages.map((language) => {
                 return <Badge key={language}>{language}</Badge>;
               })}
             </div>
@@ -196,7 +195,7 @@ export default function Page() {
           <Section>
             <h2 className="text-xl font-bold">Skills</h2>
             <div className="flex flex-wrap gap-1">
-              {data.skills.map((skill) => {
+              {resumeData.skills.map((skill) => {
                 return <Badge key={skill}>{skill}</Badge>;
               })}
             </div>
@@ -205,7 +204,7 @@ export default function Page() {
           <Section className="print-force-new-page scroll-mb-16">
             <h2 className="text-xl font-bold">Projects</h2>
             <div className="-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3">
-              {data.projects.map((project) => {
+              {resumeData.projects.map((project) => {
                 return (
                   <ProjectCard
                     key={project.title}
@@ -223,10 +222,10 @@ export default function Page() {
         <CommandMenu
           links={[
             {
-              url: data.personalWebsiteUrl,
+              url: resumeData.personalWebsiteUrl,
               title: "Personal Website",
             },
-            ...data.contact.social.map((socialMediaLink) => ({
+            ...resumeData.contact.social.map((socialMediaLink) => ({
               url: socialMediaLink.url,
               title: socialMediaLink.name,
             })),
